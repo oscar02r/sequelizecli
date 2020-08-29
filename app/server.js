10d2d0ba674fa76ee964f5e2ef038d9e93ed4259
oscar02r <oscary.ramirez@gmail.com>
2020-08-29 23:52:14 +0200

    Config complete and connection

--- app/server.js
+++ app/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const { connection } = require('./database/db');
 
 const app = new express();
-//const sequelize = require('./database/db');
-//require('./database/association');
+
 
 //Setting
 const PORT = process.env.PORT || 5000;
@@ -12,13 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 
-/*//Paths
-app.use('/api/users', require('./routes/users'));
-app.use('/api/posts', require('./routes/posts'));
-app.use('/api/addresses', require('./routes/addresses'));
-*/
 app.listen(PORT, () => {
     console.log(`La app ha arrancado en hhtp://localhost:${PORT}`);
-
+    connection.sync({force: true})
+    .then(()=>{
+        console.log('Connection success');
+    });
     
 });
